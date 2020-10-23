f5752e1cebda3e73ce456348c2ef9d25072e53d0
Davi Santos <davi-rocha2@hotmail.com>
2020-10-22 22:56:14 -0300

    handling error boundaries. Added React Lazy and Suspense

---
+++ client/src/components/error-boundary/error-boundary.component.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import {
+	 ErrorImageOverlay, 
+	 ErrorImageContainer, 
+	 ErrorImageText
+	} from './error-boundary.styles';
+
+class ErrorBoundary extends React.Component {
+
+	constructor() {
+		super();
+
+		this.state = {
+			hasErrored: false
+		}
+	}
+
+	static getDerivedStateFromError(error) {
+
+		//process the error
+		return { hasErrored: true }
+	}
+
+	componentDidCatch(error, info) {
+		console.log(error);
+	}
+
+	render() {
+		if (this.state.hasErrored) {
+			return (
+				<ErrorImageOverlay>
+					<ErrorImageContainer imageUrl='https://i.imgur.com/Q2BAOd2.png'/>
+					<ErrorImageText> Sorry, something went wrong </ErrorImageText>
+				</ErrorImageOverlay>
+			)
+		}
+
+		return this.props.children;
+
+	}
+
+}
+
+export default ErrorBoundary;
